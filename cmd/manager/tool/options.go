73c55e1cd04b7105c45c678e165f454b518a6cca
Vadim Eisenberg <vadime@il.ibm.com>
2021-05-19 13:12:58 +0300

    remove unused options

--- cmd/manager/tool/options.go
+++ cmd/manager/tool/options.go
@@ -4,43 +4,23 @@
 package tool
 
 import (
-	"context"
-
 	"github.com/spf13/pflag"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/client-go/kubernetes"
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
 )
 
 var log = logf.Log.WithName("cmd")
 
-// PolicySpecSyncOptions for command line flag parsing
-type PolicySpecSyncOptions struct {
-	ClusterName               string
-	ClusterNamespace          string
-	HubConfigFilePathName     string
+// HubOfHubsSpecSyncOptions for command line flag parsing
+type HubOfHubsSpecSyncOptions struct {
+	HubConfigFilePathName string
 }
 
 // Options default value
-var Options = PolicySpecSyncOptions{}
+var Options = HubOfHubsSpecSyncOptions{}
 
 // ProcessFlags parses command line parameters into Options
 func ProcessFlags() {
 	flag := pflag.CommandLine
-
-	flag.StringVar(
-		&Options.ClusterName,
-		"cluster-name",
-		Options.ClusterName,
-		"Name of this endpoint.",
-	)
-
-	flag.StringVar(
-		&Options.ClusterNamespace,
-		"cluster-namespace",
-		Options.ClusterNamespace,
-		"Cluster Namespace of this endpoint in hub.",
-	)
 
 	flag.StringVar(
 		&Options.HubConfigFilePathName,
@@ -48,12 +28,4 @@ func ProcessFlags() {
 		Options.HubConfigFilePathName,
 		"Configuration file pathname to hub kubernetes cluster",
 	)
-}
-
-// DeleteClusterNs deletes the cluster namespace on managed cluster if not exists
-func DeleteClusterNs(client *kubernetes.Interface, ns string) error {
-	if ns != "" {
-		return (*client).CoreV1().Namespaces().Delete(context.TODO(), ns, metav1.DeleteOptions{})
-	}
-	return nil
 }
