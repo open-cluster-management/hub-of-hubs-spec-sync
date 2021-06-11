b8849ee1f4fe971b31c6418b9aba83c56f39a19b
Vadim Eisenberg <vadime@il.ibm.com>
2021-06-11 22:54:32 +0300

    refactoring: extract functions from Reconcile()

--- pkg/controller/generic_spec_to_db_reconciler.go
+++ pkg/controller/generic_spec_to_db_reconciler.go
@@ -42,74 +42,19 @@ func (r *genericSpecToDBReconciler) Reconcile(request ctrl.Request) (ctrl.Result
 	reqLogger.Info(fmt.Sprintf("Reconciling %s ...", r.tableName))
 
 	ctx := context.Background()
-
-	instance := r.createInstance()
-	err := r.client.Get(ctx, request.NamespacedName, instance)
+	instance, err := r.processCR(ctx, request, reqLogger)
 	if err != nil {
-		if apierrors.IsNotFound(err) {
-			// the instance on hub was deleted, update all the matching instances in the database as deleted
-			err = r.deleteFromTheDatabase(request.Name, request.Namespace)
-			if err != nil {
-				reqLogger.Error(err, "Delete failed")
-				return ctrl.Result{}, err
-			}
-
-			reqLogger.Info("Reconciliation complete.")
-
-			return ctrl.Result{}, nil
-		}
-		// Error reading the object - requeue the request.
-		reqLogger.Error(err, "Failed to get the instance from hub...")
-
-		return ctrl.Result{}, err
+		return ctrl.Result{Requeue: true, RequeueAfter: requeuePeriodSeconds * time.Second}, err
 	}
 
-	instanceID := instance.GetUID()
-
-	if instance.GetDeletionTimestamp().IsZero() {
-		if !containsString(instance.GetFinalizers(), r.finalizerName) {
-			reqLogger.Info("Adding finalizer")
-			controllerutil.AddFinalizer(instance, r.finalizerName)
-			if err := r.client.Update(ctx, instance); err != nil {
-				return ctrl.Result{Requeue: true, RequeueAfter: requeuePeriodSeconds * time.Second}, err
-			}
-		}
-	} else {
-		if containsString(instance.GetFinalizers(), r.finalizerName) {
-			// the policy is being deleted, update all the matching policies in the database as deleted
-			if err := r.deleteFromTheDatabase(request.Name, request.Namespace); err != nil {
-				reqLogger.Error(err, "Delete failed")
-				return ctrl.Result{}, err
-			}
-			reqLogger.Info("Removing finalizer")
-			controllerutil.RemoveFinalizer(instance, r.finalizerName)
-			if err = r.client.Update(ctx, instance); err != nil {
-				return ctrl.Result{Requeue: true, RequeueAfter: requeuePeriodSeconds * time.Second}, err
-			}
-		}
+	if instance == nil {
 		reqLogger.Info("Reconciliation complete.")
-		return ctrl.Result{}, nil
+		return ctrl.Result{}, err
 	}
 
-	cleanInstance(instance)
-
-	instanceInTheDatabase := r.createInstance()
-	err = r.databaseConnectionPool.QueryRow(ctx,
-		fmt.Sprintf("SELECT payload FROM spec.%s WHERE id = $1", r.tableName),
-		string(instanceID)).Scan(&instanceInTheDatabase)
-
-	if errors.Is(err, pgx.ErrNoRows) {
-		reqLogger.Info("The instance with the current UID does not exist in the database, inserting...")
-		_, err := r.databaseConnectionPool.Exec(ctx,
-			fmt.Sprintf("INSERT INTO spec.%s (id,payload) values($1, $2::jsonb)", r.tableName),
-			string(instanceID), &instance)
-		if err != nil {
-			reqLogger.Error(err, "Insert failed")
-		} else {
-			reqLogger.Info("The instance has been inserted into the database...Reconciliation complete.")
-		}
-
-		return ctrl.Result{}, err
+	instanceInTheDatabase, err := r.processInstanceInTheDatabase(ctx, instance, reqLogger)
+	if err != nil {
+		return ctrl.Result{Requeue: true, RequeueAfter: requeuePeriodSeconds * time.Second}, err
 	}
 
 	if !r.areEqual(instance, instanceInTheDatabase) {
@@ -117,7 +62,7 @@ func (r *genericSpecToDBReconciler) Reconcile(request ctrl.Request) (ctrl.Result
 
 		_, err := r.databaseConnectionPool.Exec(ctx,
 			fmt.Sprintf("UPDATE spec.%s SET payload = $1 WHERE id = $2", r.tableName),
-			&instance, string(instanceID))
+			&instance, string(instance.GetUID()))
 		if err != nil {
 			reqLogger.Error(err, "Update failed")
 			return ctrl.Result{}, err
@@ -129,7 +74,80 @@ func (r *genericSpecToDBReconciler) Reconcile(request ctrl.Request) (ctrl.Result
 	return ctrl.Result{}, err
 }
 
-func cleanInstance(instance object) {
+func (r *genericSpecToDBReconciler) processCR(ctx context.Context, request ctrl.Request,
+	log logr.Logger) (object, error) {
+	instance := r.createInstance()
+	err := r.client.Get(ctx, request.NamespacedName, instance)
+	if err != nil {
+		if apierrors.IsNotFound(err) {
+			// the instance on hub was deleted, update all the matching instances in the database as deleted
+			err = r.deleteFromTheDatabase(request.Name, request.Namespace, log)
+			if err != nil {
+				log.Error(err, "Delete failed")
+				return nil, err
+			}
+
+			log.Info("Reconciliation complete.")
+
+			return nil, nil
+		}
+
+		log.Error(err, "Failed to get the instance from hub...")
+		return nil, err
+	}
+
+	if instance.GetDeletionTimestamp().IsZero() {
+		if !containsString(instance.GetFinalizers(), r.finalizerName) {
+			log.Info("Adding finalizer")
+			controllerutil.AddFinalizer(instance, r.finalizerName)
+			if err := r.client.Update(ctx, instance); err != nil {
+				return instance, err
+			}
+		}
+	} else {
+		if containsString(instance.GetFinalizers(), r.finalizerName) {
+			// the policy is being deleted, update all the matching policies in the database as deleted
+			if err := r.deleteFromTheDatabase(request.Name, request.Namespace, log); err != nil {
+				log.Error(err, "Delete failed")
+				return nil, err
+			}
+			log.Info("Removing finalizer")
+			controllerutil.RemoveFinalizer(instance, r.finalizerName)
+			if err = r.client.Update(ctx, instance); err != nil {
+				return nil, err
+			}
+		}
+		return nil, nil
+	}
+
+	return cleanInstance(instance), nil
+}
+
+func (r *genericSpecToDBReconciler) processInstanceInTheDatabase(ctx context.Context, instance object,
+	log logr.Logger) (object, error) {
+	instanceInTheDatabase := r.createInstance()
+	err := r.databaseConnectionPool.QueryRow(ctx,
+		fmt.Sprintf("SELECT payload FROM spec.%s WHERE id = $1", r.tableName),
+		string(instance.GetUID())).Scan(&instanceInTheDatabase)
+
+	if errors.Is(err, pgx.ErrNoRows) {
+		log.Info("The instance with the current UID does not exist in the database, inserting...")
+		_, err := r.databaseConnectionPool.Exec(ctx,
+			fmt.Sprintf("INSERT INTO spec.%s (id,payload) values($1, $2::jsonb)", r.tableName),
+			string(instance.GetUID()), &instance)
+		if err != nil {
+			log.Error(err, "Insert failed")
+		} else {
+			log.Info("The instance has been inserted into the database...Reconciliation complete.")
+		}
+
+		return instance, err
+	}
+
+	return instanceInTheDatabase, nil
+}
+
+func cleanInstance(instance object) object {
 	instance.SetUID("")
 	instance.SetResourceVersion("")
 	instance.SetManagedFields(nil)
@@ -139,19 +157,20 @@ func cleanInstance(instance object) {
 	instance.SetClusterName("")
 
 	delete(instance.GetAnnotations(), "kubectl.kubernetes.io/last-applied-configuration")
+
+	return instance
 }
 
-func (r *genericSpecToDBReconciler) deleteFromTheDatabase(name, namespace string) error {
-	reqLogger := r.log.WithValues("Request.Namespace", namespace, "Request.Name", name)
+func (r *genericSpecToDBReconciler) deleteFromTheDatabase(name, namespace string, log logr.Logger) error {
 	// the policy on hub was deleted, update all the matching policies in the database as deleted
-	reqLogger.Info("Instance was deleted, update the deleted field in the database...")
+	log.Info("Instance was deleted, update the deleted field in the database...")
 
 	_, err := r.databaseConnectionPool.Exec(context.Background(),
 		fmt.Sprintf(`UPDATE spec.%s SET deleted = true WHERE payload -> 'metadata' ->> 'name' = $1 AND
 			     payload -> 'metadata' ->> 'namespace' = $2 AND deleted = false`, r.tableName), name, namespace)
 
 	if err == nil {
-		reqLogger.Info("Instance has been updated as deleted in the database...")
+		log.Info("Instance has been updated as deleted in the database...")
 	}
 
 	return err
