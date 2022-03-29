081f655cec59398b4470a7d486a2edc0251158e3
clyang82 <chuyang@redhat.com>
2022-03-29 15:37:53 +0800

    generalize the annotation name for local resources
    Signed-off-by: clyang82 <chuyang@redhat.com>

--- pkg/controller/generic_spec_to_db_reconciler.go
+++ pkg/controller/generic_spec_to_db_reconciler.go
@@ -31,8 +31,7 @@ type genericSpecToDBReconciler struct {
 
 const (
 	requeuePeriodSeconds = 5
-	hubOfHubsLocalPolicy = "hub-of-hubs.open-cluster-management.io/local-policy"
-	rootPolicy           = "policy.open-cluster-management.io/root-policy"
+	hubOfHubsLocalPolicy = "hub-of-hubs.open-cluster-management.io/local-resource"
 )
 
 func (r *genericSpecToDBReconciler) Reconcile(ctx context.Context, request ctrl.Request) (ctrl.Result, error) {
